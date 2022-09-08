10e977642c778d8913a32df937716ac3a48e57e2
john-chang94 <j.chang94@yahoo.com>
2022-09-08 10:54:34 -0700

    product purchase

--- src/ProductDesc.js
+++ src/ProductDesc.js
@@ -1,38 +1,41 @@
-import React from 'react';
+import React from "react";
 
-import caret from './images/caret.svg';
+import caret from "./images/caret.svg";
 
 export const ProductDesc = ({ name, description, rating, features, shade }) => (
   <section className="elc-product-desc">
     <h2 className="elc-product-desc__title">{name}</h2>
     <p className="elc-product-desc__subtitle">{description}</p>
-    <figure className='elc-product-images'>
+    <figure className="elc-product-images">
       <img src={rating} alt="Product Rating" />
       <img src={caret} alt="View All Ratings" />
     </figure>
     <section className="elc-features">
       <div>
-        <h4 className='elc-features__label'>Benefits</h4>
-        <p className='elc-features__desc'>{features.benefits}</p>
+        <h4 className="elc-features__label">Benefits</h4>
+        <p className="elc-features__desc">{features.benefits}</p>
       </div>
       <div>
-        <h4 className='elc-features__label'>Finish</h4>
-        <p className='elc-features__desc'>{features.finish}</p>
+        <h4 className="elc-features__label">Finish</h4>
+        <p className="elc-features__desc">{features.finish}</p>
       </div>
       <div>
-        <h4 className='elc-features__label'>Coverage</h4>
-        <p className='elc-features__desc'>{features.coverage}</p>
+        <h4 className="elc-features__label">Coverage</h4>
+        <p className="elc-features__desc">{features.coverage}</p>
       </div>
       <div>
-        <h4 className='elc-features__label'>Key Ingredients</h4>
-        <p className='elc-features__desc'>
-          {features.keyIngredients} <a href="http://www.clinique.com">More</a>
+        <h4 className="elc-features__label">Key Ingredients</h4>
+        <p className="elc-features__desc">
+          {features.keyIngredients}{" "}
+          <a className="elc-features__expand" href="http://www.clinique.com">
+            More
+          </a>
         </p>
       </div>
     </section>
-    <section className='elc-shade'>
-      <span className='elc-shade__name'>{shade.name}</span>
-      <div className='elc-shade__color'></div>
+    <section className="elc-shade">
+      <span className="elc-shade__name">{shade.name}</span>
+      <div className="elc-shade__color"></div>
     </section>
   </section>
 );
--- src/ProductPurchase.js
+++ src/ProductPurchase.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import caret from './images/caret.png';
-import minus from './images/minus.png';
-import plus from './images/plus.png';
+import caret from "./images/caret.png";
+import minus from "./images/minus.png";
+import plus from "./images/plus.png";
 
-export const ProductPurchase = ({ price }) =>
-  <section>
-    <div>
-      Quantity <img src={minus} alt="Decrease Quantity"/> 1 <img src={plus} alt="Increase Quantity"/>
+export const ProductPurchase = ({ price }) => (
+  <section className="elc-product-purchase">
+    <div className="elc-product-purchase__container">
+      <div className="elc-product-purchase__wrapper">
+        <p>Quantity</p>
+        <img src={minus} alt="Decrease Quantity" />
+        <p>1</p>
+        <img src={plus} alt="Increase Quantity" />
+      </div>
+      <div className="elc-product-purchase__wrapper">
+        <p>One Time Purchase</p>
+        <img src={caret} alt="Select Frequency" />
+      </div>
     </div>
-    <div>
-      One Time Purchase  <img src={caret} alt="Select Frequency"/>
-    </div>
-    <div>{price}</div>
-    <button name="AddToBag">Add To Bag</button>
+    <div className="elc-product-purchase__price">{price}</div>
+    <button className="elc-product-purchase__btn" name="AddToBag">
+      Add To Bag
+    </button>
   </section>
+);
